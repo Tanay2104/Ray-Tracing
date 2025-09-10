2bdb8e40d66358f5fb923e260a34e1bbb4fa3b1c
Tanay Jha <tanay2104@gmail.com>
2025-09-11 03:09:51 +0530

    Added color and ray classes

---
+++ color.h
@@ -0,0 +1,21 @@
+#ifndef COLOR_H
+#define COLOR_H
+
+#include "vec3.h"
+#include <iostream>
+
+using color = vec3;
+void write_color(std::ostream& out, const color& pixel_colour) {
+    auto r = pixel_colour.x();
+    auto g = pixel_colour.y();
+    auto b = pixel_colour.z();
+
+    short rbyte = short(255.999 * r);
+    short gbyte = short(255.999 * r);
+    short bbyte = short(255.999 * r);
+
+
+    out << rbyte << ' ' << gbyte << ' ' << bbyte << '\n';
+}
+
+#endif
