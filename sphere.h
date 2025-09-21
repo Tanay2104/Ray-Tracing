4b067227e8c2740b77a2429e56959d335cc585fd
Tanay Jha <tanay2104@gmail.com>
2025-09-22 01:50:19 +0530

    Added interval class and it's functionality

--- sphere.h
+++ sphere.h
@@ -11,7 +11,7 @@ class sphere: public hittable {
     public:
         sphere(const point3& c, double r) : center(c), radius(std::fmax(0, r)) {}
 
-        bool hit(const ray& r, double ray_tmin, double ray_tmax, hit_record& rec) const override {
+        bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
             vec3 oc  = center - r.origin();
             auto a = r.direction().length_squared();
             auto h = dot(r.direction(), oc);
@@ -25,9 +25,9 @@ class sphere: public hittable {
 
             // Find the nearest root that lies in the acceptable range
             auto root = (h - sqrtd) / a;
-            if (root <= ray_tmin || root >= ray_tmax) {
+            if (!ray_t.surrounds(root)) {
                 root = (h + sqrtd) / a;
-                if (root <= ray_tmin || root >= ray_tmax)
+                if (!ray_t.surrounds(root))
                     return false;
             }
 
